0e1f9af1371a98211bf87656bd349ba458245364
Chris Sotraidis <56282582+chrissotraidis@users.noreply.github.com>
2026-08-15 13:59:15 -0500

    Remove stray spirv-cross build output; ignore /build/

--- build/spirv-cross/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chrissotraidis/GitHub/dinopad/ref/dino-recomp/lib/rt64/src/contrib/spirv-cross
-# Build directory: /Users/chrissotraidis/GitHub/dinopad/build/spirv-cross
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
